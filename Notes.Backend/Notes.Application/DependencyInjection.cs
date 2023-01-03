aa506838671067504e0f7a6ab0d9f9b35786045c
SALAVOZ <salik.abdulin@mail.ru>
2023-01-03 17:10:17 +0400

    created validation

--- Notes.Backend/Notes.Application/DependencyInjection.cs
+++ Notes.Backend/Notes.Application/DependencyInjection.cs
@@ -3,6 +3,8 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 
 namespace Notes.Application
 {
@@ -11,6 +13,8 @@ namespace Notes.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[] {Assembly.GetExecutingAssembly()});
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
             return services;
         }
     }
---
+++ Notes.Backend/Notes.WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,6 @@
+﻿namespace Notes.WebApi.Middleware
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+    }
+}
